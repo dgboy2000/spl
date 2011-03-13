adb49bd497b8e74c9fdd28a21b309eb7614c1ae5
Danny Goodman <goodman.danny@gmail.com>
2011-03-12 19:11:36 -0800

    fix bug in allocating losses

--- AlternateConvexSearch/svm_struct_latent_spl.c
+++ AlternateConvexSearch/svm_struct_latent_spl.c
@@ -692,6 +692,7 @@ double *convert_from_svector(SVECTOR *svec,int size) {
 }
 
 double get_novelty(EXAMPLE *ex, long exNum, STRUCTMODEL *sm, STRUCT_LEARN_PARM *sparm) {
+	printf ("calling get novelty\n");
   int numPairs;  
   long j;
 
@@ -700,7 +701,6 @@ double get_novelty(EXAMPLE *ex, long exNum, STRUCTMODEL *sm, STRUCT_LEARN_PARM *
 	
 	double *psi_h_star;
 	double **psi_h_y_hats;
-	double losses[numPairs];
   
   psi_h_star_sparse = psi(ex[exNum].x, ex[exNum].y, ex[exNum].h, sm, sparm);
   psi_h_star = convert_from_svector(psi_h_star_sparse,sm->sizePsi);
@@ -712,13 +712,15 @@ double get_novelty(EXAMPLE *ex, long exNum, STRUCTMODEL *sm, STRUCT_LEARN_PARM *
     psi_h_y_hats[j] = convert_from_svector(psi_h_y_hats_sparse[j],sm->sizePsi);
   }
 
+	double losses[numPairs];
   get_all_losses(ex, exNum, losses, sm, sparm);
   
   // double novelty = compute_delta_w(sm->w,psi_h_star,psi_h_y_hats,losses,sm->sizePsi,numPairs);
 
   for(j=0;j<numPairs;j++) {
+		printf ("Freeing vector %d\n", j);
     free_svector(psi_h_y_hats_sparse[j]);
-		free(psi_h_y_hats[j]);
+		// free(psi_h_y_hats[j]);
   }  
   free(psi_h_y_hats_sparse);
   free(psi_h_y_hats);
