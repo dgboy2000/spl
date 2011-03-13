cb228d54408efd39993fc6236b68eada3bec1a12
Danny Goodman <goodman.danny@gmail.com>
2011-03-12 22:34:09 -0800

    fixed the mosek code and wrote a simple test example

--- AlternateConvexSearch/svm_struct_latent_spl.c
+++ AlternateConvexSearch/svm_struct_latent_spl.c
@@ -674,25 +674,28 @@ int check_acs_convergence(int *prev_valid_examples, int *valid_examples, long m)
 	return nValid;
 }*/
 
+/* Convert a single one-indexed sparse vector into a full vector of doubles. */
 double *convert_from_svector(SVECTOR *svec,int size) {
-  double *v = malloc(sizeof(double)*size);
+  double *v = calloc (1, sizeof(double)*size);
   SVECTOR *f;
-  long j;  
-  for (f=svec;f;f=f->next) {
-		j = 0;
-		while (1) {
-			if(!f->words[j].wnum)
-				break;
-      v[f->words[j].wnum] = f->words[j].weight;
-      j++;
+  long j;
+	int pos;
+	
+	f = svec;
+	j = 0;
+	pos = f->words[j].wnum;
+	while (pos)
+		{
+			assert (pos <= size);
+			v[pos-1] = f->words[j].weight;
+			++j;
+			pos = f->words[j].wnum;			
 		}
-	}
 
   return v;
 }
 
 double get_novelty(EXAMPLE *ex, long exNum, STRUCTMODEL *sm, STRUCT_LEARN_PARM *sparm) {
-	printf ("calling get novelty\n");
   int numPairs;  
   long j;
 
@@ -716,12 +719,11 @@ double get_novelty(EXAMPLE *ex, long exNum, STRUCTMODEL *sm, STRUCT_LEARN_PARM *
   get_all_losses(ex, exNum, losses, sm, sparm);
   
   double novelty = compute_delta_w(sm->w,psi_h_star,psi_h_y_hats,losses,sm->sizePsi,numPairs);
-  //double novelty = 0.0;
+  // double novelty = 0.0;
   
   for(j=0;j<numPairs;j++) {
-		printf ("Freeing vector %d\n", j);
     free_svector(psi_h_y_hats_sparse[j]);
-		// free(psi_h_y_hats[j]);
+		free(psi_h_y_hats[j]);
   }  
   free(psi_h_y_hats_sparse);
   free(psi_h_y_hats);
@@ -821,6 +823,8 @@ sortStruct *get_example_scores(long m, double C, SVECTOR **fycache, EXAMPLE *ex,
 		free_svector(fy);
 		free_svector(fybar);
 	}
+
+  print_mosek_stats ();
 
 	qsort(exampleScores,m,sizeof(sortStruct),&compar);
   return(exampleScores);
---
+++ AlternateConvexSearch/test_mosek.c
@@ -0,0 +1,6 @@
+#include "mosek_api.h"
+
+int main(int argc, char* argv[])
+{
+  test_mosek ();
+}
