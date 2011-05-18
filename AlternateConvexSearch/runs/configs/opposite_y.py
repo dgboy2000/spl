973d6ddc413e012eee044964a3e873fa3fce770d
Danny Goodman <goodman.danny@gmail.com>
2011-05-18 00:54:17 -0700

    Modify the batch params to run the new alg.

--- AlternateConvexSearch/runs/configs/opposite_y.py
+++ AlternateConvexSearch/runs/configs/opposite_y.py
@@ -5,16 +5,17 @@ params = {
     'seeds' : [ '0000', '1000', '2000', '3000' ],
     
     'name' : 'opposite_y',
-    'extension' : '2011-05-03', # Set this to 'datetime' to make a new run or a specific string to continue an old one
+    'extension' : '2011-05-17', # Set this to 'datetime' to make a new run or a specific string to continue an old one
     'description' : """
-This control run covers cccp, slack, and uncertainty for the existing algorithms.
+This control run covers cccp, slack, uncertainty, and shannon learning algorithms.
         """,
     
     # Each entry is a pair of parameter sets; one set is for training, and the other is the corresponding set for inference
     'param_pairs' : {
-        'cccp' : ['-c 150 -k 0 -m 1.3 --t 1', ''],
-        'slack' : ['-c 150 -k 100 -m 1.3 -f 0.55 --t 1', ''],
-        'uncertainty' : ['-c 150 -k 100 -m 1.3 -f 0.55 -u 1 --t 1', '']
+        # 'cccp' : ['-c 150 -k 0 -m 1.3 --t 1', ''],
+        # 'slack' : ['-c 150 -k 100 -m 1.3 -f 0.55 --t 1', ''],
+        'uncertainty' : ['-c 150 -k 100 -m 1.3 -f 0.55 -x -2.0 --t 1', ''],
+        'shannon' : ['-c 150 -k 100 -m 1.3 -f 0.55 -x 1.0 --t 1', '']
     },
     
     'raise_errors' : True
